1f5e4aeb9716127851827570851c770d1018958f
Ajjeroni <adanjeronimo213@gmail.com>
2024-04-12 20:50:08 -0700

    deleted folder

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+first_name = "aDaN"
+last_name = "jERONimO"
+
+full_name = f"{first_name} {last_name}"
+
+greeting = f"Hello {full_name.lower().title()}!"
+
+print(greeting)
+
+print("Languages:\n\tPython\n\tC\n\tJavascript")
+
+url = "https://www.ledr.com/colours/white.htm"
+
+print(url.removeprefix('https://'))
