26c9a6076d1185b8911478cd8488909d67ed704d
Adan Jeronimo <127287490+ajjeroni@users.noreply.github.com>
2024-04-12 20:47:07 -0700

    Add files via upload

---
+++ python_work/famous_quote.py
@@ -0,0 +1,10 @@
+famous_person = "Jesus Christ"
+famous_person_quote = "'the first shall be last'"
+
+famous_quote = f"{famous_person} once said, {famous_person_quote.title()}."
+
+print(famous_quote)
+
+filename = 'python.txt'
+
+print(filename.removesuffix('.txt'))
