bea79abdada298d16763dcef96408303f0db3412
Ajjeroni <adanjeronimo213@gmail.com>
2024-04-13 23:16:48 -0700

    worked some more on list methods

---
+++ python_list_two.py
@@ -0,0 +1,11 @@
+colors = ['blue','white','red','green','yellow','purple','orange']
+print(colors)
+
+colors.sort()
+print(colors)
+
+# colors.sort(reverse=True)
+# print(colors)
+
+print('\nHere is orginal')
+print(sorted(colors, reverse=True))
